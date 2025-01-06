a777df4c1a1823eab5d28d223c74a94280384414
Braynin <brayhannb@gmail.com>
2025-01-06 02:15:58 -0500

    avanzanding servicios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind"; // Importa Tailwind
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(), // Mantén React
+    tailwind(), // Agrega Tailwind
+  ],
 });
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}", // Ajusta las rutas según tu estructura de proyecto
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
