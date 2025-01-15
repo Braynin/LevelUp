c23350747776539504a76ca0df9bde8cc9257542
Braynin <brayhannb@gmail.com>
2025-01-14 21:03:28 -0500

    implementando TailWind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
 // tailwind.config.js
 module.exports = {
   content: [
-    "./src/**/*.{astro,html,js,jsx,ts,tsx}", // Ajusta las rutas según tu estructura de proyecto
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,css}", // Ajusta las rutas según tu estructura de proyecto
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeInOut1: "fadeInOut1 6s ease-in-out infinite",
+        fadeInOut2: "fadeInOut2 6s ease-in-out infinite",
+      },
+      keyframes: {
+        fadeInOut1: {
+          "0%, 40%": { opacity: "1", zIndex: "10" },
+          "45%, 100%": { opacity: "0", zIndex: "0" },
+        },
+        fadeInOut2: {
+          "0%, 45%": { opacity: "0", zIndex: "0" },
+          "50%, 90%": { opacity: "1", zIndex: "10" },
+          "95%, 100%": { opacity: "0", zIndex: "0" },
+        },
+      },
+    },
   },
   plugins: [],
 };
