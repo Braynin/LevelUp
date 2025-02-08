e929f797ffa5119e6ae23268523fe59d19afd02c
Braynin <brayhannb@gmail.com>
2025-02-07 19:57:42 -0500

    no she

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind"; // Importa Tailwind
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+
   integrations: [
     react(), // Mantén React
     tailwind(), // Agrega Tailwind
